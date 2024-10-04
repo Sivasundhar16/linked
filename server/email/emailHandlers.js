53e067b1df9ed88b9bab8272da9e21d8c7932264
Sivasundhar <vssiva18@gmail.com>
2024-10-04 23:08:36 +0530

    Testing git for updating some changes

--- server/email/emailHandlers.js
+++ server/email/emailHandlers.js
@@ -1,19 +1,20 @@
-import { MailtrapClient, sender } from "../controllers/mailtrap";
+import { MailtrapClient, sender } from "../controllers/mailtrap.js";
+import { createWelcomeEmailTemplate } from "./emailTemplates.js";
 
 export const sendWelcomeEmail = async (email, name, profileUrl) => {
   const recipient = [{ email }];
+
+  try {
+    const response = await MailtrapClient({
+      from: sender,
+      to: recipient,
+      subject: "Welcome to LinkedIn",
+      html: createWelcomeEmailTemplate(name, profileUrl),
+      category: "welcome",
+    });
+
+    console.log("Email send Sucessfully", response);
+  } catch (error) {
+    throw error;
+  }
 };
-
-try {
-  const response = await MailtrapClient({
-    from: sender,
-    to: recipient,
-    subject: "Welcome to LinkedIn",
-    html: createWelcomeEmailTemplate(name, profileUrl),
-    category: "welcome",
-  });
-
-  console.log("Email send Sucessfully", response);
-} catch (error) {
-  throw error;
-}
