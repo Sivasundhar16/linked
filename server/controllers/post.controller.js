977c4ac5f2a47b809378afe333e93ee4bd55ae97
Sivasundhar <vssiva18@gmail.com>
2024-10-09 23:35:54 +0530

    Created a route for adding post and pictures

--- server/controllers/post.controller.js
+++ server/controllers/post.controller.js
@@ -1,3 +1,4 @@
+import cloudinary from "../cloud/cloudnary.js";
 import Post from "../models/post.model.js";
 
 export const getFeedPost = async (req, res) => {
@@ -9,6 +10,31 @@ export const getFeedPost = async (req, res) => {
     res.status(200).json(posts);
   } catch (error) {
     console.log("Error in Getfeedpost controller", error.message);
+    res.status(500).json({ message: "Server error" });
+  }
+};
+
+export const createPost = async (req, res) => {
+  try {
+    const { content, image } = req.body;
+    let newPost;
+    if (image) {
+      const imageResult = await cloudinary.uploader.upload(image);
+      newPost = new Post({
+        author: req.user._id,
+        content,
+        image: imageResult.secure_url,
+      });
+    } else {
+      newPost = new Post({
+        author: req.user._id,
+        content,
+      });
+    }
+    await newPost.save();
+    res.status(201).json({ newPost });
+  } catch (error) {
+    console.log("Error in createpost controller", error.message);
     res.status(500).json({ message: "Server error" });
   }
 };
