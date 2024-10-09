bc4a73b1273039751b5550c3f1560a95ed8047d3
Sivasundhar <vssiva18@gmail.com>
2024-10-09 23:23:38 +0530

    created Feed Post route

---
+++ server/models/notification.model.js
@@ -0,0 +1,32 @@
+import mongoose from "mongoose";
+
+const notificationSchema = new mongoose.Schema(
+  {
+    recipient: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      require: "true",
+    },
+    type: {
+      type: String,
+      require: true,
+      enum: ["like", "comment", "connectionAccepted"],
+    },
+    relatedUser: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+    relatedPost: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Post",
+    },
+    read: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  { timestamps: true }
+);
+
+const Notification = mongoose.model("Post", notificationSchema);
+export default Notification;
--- server/routes/post.routs.js
+++ server/routes/post.routs.js
@@ -1,5 +1,10 @@
 import express from "express";
+import { protectRoute } from "../middleware/auth.middleware.js";
+import { getFeedPost } from "../controllers/post.controller.js";
 
 const router = express.Router();
+
+router.get("/", protectRoute, getFeedPost);
+router.post("/create", protectRoute, createPost);
 
 export default router;
