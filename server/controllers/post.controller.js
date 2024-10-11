df70ceed612ddc21afbfd5d9a4c1371c69e0e046
Sivasundhar <vssiva18@gmail.com>
2024-10-11 13:02:35 +0530

    Added getpost by Id

--- server/controllers/post.controller.js
+++ server/controllers/post.controller.js
@@ -38,3 +38,46 @@ export const createPost = async (req, res) => {
     res.status(500).json({ message: "Server error" });
   }
 };
+
+export const deletePost = async (req, res) => {
+  try {
+    const postId = req.params.id;
+    const userId = req.user._id;
+    const post = await Post.findById(postId);
+
+    if (!post) {
+      return res.status(404).json({ message: "Post Not found" });
+    }
+    if (post.author.toString() !== userId.toString()) {
+      return res
+        .status(403)
+        .json({ message: "You are not allowed to delete this post" });
+    }
+
+    //delete the image from cloudnary
+    if (post.image) {
+      await cloudinary.uploader.destroy(
+        post.image.split("/").pop().split(".")[0]
+      );
+    }
+
+    await Post.findByIdAndDelete(postId);
+    res.status(200).json({ message: "Post Deleted" });
+  } catch (error) {
+    console.log("Error in Deleting", error.message);
+    res.status(400).json({ message: "Error in Deleting" });
+  }
+};
+
+export const getPostById = async (req, res) => {
+  try {
+    const postId = req.param.id;
+    const post = await Post.findById(postId)
+      .populate("author", "name username profilePicture headline")
+      .populate("comments.user", "name profilePicture username headline");
+    res.status(200).json(post);
+  } catch (error) {
+    console.log("Error in getPostbyID ", error.message);
+    res.status(500).json({ message: "Server Error" });
+  }
+};
--- server/routes/post.routs.js
+++ server/routes/post.routs.js
@@ -1,10 +1,17 @@
 import express from "express";
 import { protectRoute } from "../middleware/auth.middleware.js";
-import { getFeedPost } from "../controllers/post.controller.js";
+import {
+  getFeedPost,
+  createPost,
+  deletePost,
+  getPostById,
+} from "../controllers/post.controller.js";
 
 const router = express.Router();
 
 router.get("/", protectRoute, getFeedPost);
 router.post("/create", protectRoute, createPost);
+router.delete("/delete/:id", protectRoute, deletePost);
+router.get("/:id", protectRoute, getPostById);
 
 export default router;
