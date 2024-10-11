2c9391a95e91148f9d621095c472dc95f1fc5b94
Sivasundhar <vssiva18@gmail.com>
2024-10-11 21:17:57 +0530

    Made connection route . But not create their function

--- server/routes/connection.route.js
+++ server/routes/connection.route.js
@@ -1,5 +1,24 @@
 import express from "express";
+import { protectRoute } from "../middleware/auth.middleware.js";
+
+// import {
+//   getUserNotifications,
+//   markNotificationAsRead,
+//   deleteNotification,
+// } from "../controllers/notification.controller.js";
 
 const router = express.Router();
+
+router.post("/request/:userId", protectRoute, sendConnectioRequest);
+router.put("/request/:requestId", protectRoute, acceptConnectionRequest);
+router.put("/reject/:requestId", protectRoute, rejectConnectionRequest);
+
+////////////////////
+router.get("/requests", protectRoute, getConnectionRequests);
+
+///////////////////
+router.get("/", protectRoute, getUserNotifications);
+router.delete("/:userId", protectRoute, removeConnection);
+router.get("/status/:usedId", protectRoute, getConnectionStatus);
 
 export default router;
