0d7a2efc63bd3f483481107b69e59736ccbf8369
Sivasundhar <vssiva18@gmail.com>
2024-10-14 18:05:22 +0530

    started the frontend

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,18 @@
-import react from "react";
+import Layout from "./components/layout/Layout";
+import { Routes, Route } from "react-router-dom";
+import { SignupPage } from "./pages/auth/SignupPage";
+import { LoginPage } from "./pages/auth/LoginPage";
+import { HomePage } from "./pages/HomePage";
+
 function App() {
   return (
-    <>
-      <h1 className="text-2xl font-bold underline">Hello world!</h1>
-    </>
+    <Layout>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/signup" element={<SignupPage />} />
+        <Route path="/login" element={<LoginPage />} />
+      </Routes>
+    </Layout>
   );
 }
 
